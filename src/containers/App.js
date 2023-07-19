c6379ad9372c3a8fa724e852db0b384e9e8ef566
mythilyarasu <mythilyarasu@gmail.com>
2023-07-19 15:14:23 +0530

    updates

---
+++ src/containers/App.js
@@ -0,0 +1,52 @@
+import React ,{Component} from 'react';
+import Cardlist from '../components/Cardlist';
+import Searchbox from '../components/Searchbox';
+import Scroll from '../components/Scroll';
+import Errorboundry from '../components/Errorboundry';
+import './App.css';
+
+
+
+class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+          robots: [],
+          searchfield: ''
+
+        }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=>response.json())
+        .then(users=>{this.setState({robots:users})}) ;  
+    }
+        onSearchChange = (event) =>{
+            this.setState({searchfield: event.target.value})
+        }
+        render(){
+            const {robots,searchfield} = this.state;
+            const filteredRobots = robots.filter(robot =>{
+                return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+            })
+            return !robots.length ?
+                <h1>Loading</h1> :
+            (
+            <div className='tc'>
+              <h1 className='f1'>Robo Friends</h1>
+              <Searchbox searchChange={this.onSearchChange} />
+              <Scroll>
+                 <Errorboundry>
+                 <Cardlist robots={filteredRobots}/>
+                 </Errorboundry>
+              </Scroll>
+            </div>
+        );
+
+    }
+}
+
+
+
+export default App;
